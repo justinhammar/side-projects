9c89a59f33b82f2ad8025f95e3f9651de769439c
Justin Hammar <justin.hammar@gmail.com>
2018-09-01 18:49:47 -0500

    added pixelPygame

---
+++ pygame/pixelPygame.py
@@ -0,0 +1,16 @@
+def drawPixelMatrix(surface, pixelMatrix):
+    numberOfRows = len(pixelMatrix)
+    numberOfColumns = len(pixelMatrix[0])
+
+    maxPixelHeight = surface.get_height() // numberOfRows
+    maxPixelWidth = surface.get_width() // numberOfColumns
+    
+    pxSideLen = min(maxPixelHeight, maxPixelWidth)
+    
+    for row in xrange(0, numberOfRows):
+        for col in xrange(0, numberOfColumns):
+            surface.fill(pixelMatrix[row][col], 
+                    (col*pxSideLen, row*pxSideLen, pxSideLen, pxSideLen))
+                    
+    return surface
+    
